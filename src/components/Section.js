d7d211673086d731157a1618a356f059e62c7f12
Evgeniy Fayzrakhmanov <Jonnynsk@mail.ru>
2021-07-17 23:52:32 +0700

    some styles

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,11 +1,68 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Section = () => {
     return (
-        <div>
-            Section
-        </div>
+        <Wrap>
+            <ItemText>
+                <h1>Model S</h1>
+                <p>Order Online for Touchless Delivery</p>
+            </ItemText>
+            <ButtonGroup>
+                <LeftButton>
+                    Custom Order
+                </LeftButton>
+                <RightButton>
+                    Existing Inventory
+                </RightButton>
+            </ButtonGroup>
+        </Wrap>
+
     );
 };
 
-export default Section;
+export default Section
+
+const Wrap = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: url('/images/model-s.jpg');
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+`
+
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+`
+
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+`
+
+const LeftButton = styled.div`
+    font-size: 12px;
+    background-color rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: rgb(255, 255, 255);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    cursor: pointer;
+    
+`
+
+const RightButton = styled(LeftButton)`
+    background-color rgba(255, 255, 255, 0.65);
+    color: rgb(57, 60, 65);
+`
